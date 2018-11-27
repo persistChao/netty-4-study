afe96fabf39852c65c6c521283daa69a3643d856
suchao <persistsuchao@hotmail.com>
2018-11-27 16:12:05 +0800

    增加注释

--- src/main/java/com/answer/netty/helloworld/HelloServer.java
+++ src/main/java/com/answer/netty/helloworld/HelloServer.java
@@ -18,14 +18,18 @@ public class HelloServer {
     private static final int portNumber = 7878;
 
     public static void main(String[] args) {
+        //Worker线程用于管理线程为Boss线程服务。
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workGroup = new NioEventLoopGroup();
         try {
+            //这里 server服务端 使用ServerBootstrap  / client 客户端应该用 Bootstrap
+            //server服务端 使用.childHandler /client 客户端使用 .handler
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(bossGroup, workGroup)
             .channel(NioServerSocketChannel.class)
             .childHandler(new HelloServerInitializer());
 
+            //绑定端口 建立连接 等待客户端连接和发送消息
             ChannelFuture f = bootstrap.bind(portNumber).sync();
             f.channel().closeFuture().sync();
         } catch (InterruptedException e) {
